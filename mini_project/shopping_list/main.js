b9f65477162051620de5843ad1527cfe53242885
sungha0201 <gkgkrla030@gmail.com>
2022-02-14 17:37:07 +0900

    shopping list

--- mini_project/shopping_list/main.js
+++ mini_project/shopping_list/main.js
@@ -1,54 +1,53 @@
-const items = document.querySelector('.items');
-const input = document.querySelector('.footer__input');
-const addBtn = document.querySelector('.footer__button');
 
-function onAdd(){
-  const text = input.value;
-  if(text == ''){
-    input.focus();
-    return;
-  }
-  const item = createItem(text);
-  items.appendChild(item);
-  item.scrollIntoView({block:'center'});
-  input.value = '';
-  input.focus();
-  
-}
-function createItem(text){
-  const itemRow = document.createElement('li');
-  itemRow.setAttribute('class','item__row');
-  
-  const item = document.createElement('div');
-  item.setAttribute('class','item');
+const $items = document.querySelector('.items');
+const $input = document.querySelector('.footer__input');
+const $addBtn = document.querySelector('.footer__button');
 
-  const name = document.createElement('span');
-  name.setAttribute('class','item__name');
-  name.innerText = text;
-
-  const deleteBtn = document.createElement('button');
-  deleteBtn.setAttribute('class','item__delete');
-  deleteBtn.innerHTML = '<i class="far fa-trash-alt"></i>';
-  deleteBtn.addEventListener('click',()=>{
-    items.removeChild(itemRow);
-  });
-
-  const itemDivier = document.createElement('div');
-  itemDivier.setAttribute('class','item__divider');
-
-  item.appendChild(name);
-  item.appendChild(deleteBtn);
-
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivier);
-  return itemRow;
-}
-addBtn.addEventListener('click',()=>{
-  onAdd();
+$addBtn.addEventListener('click',()=>{
+    onAdd();
+});
+$input.addEventListener('keypress',(e)=>{
+    if(e.key === 'Enter'){
+        onAdd();
+    }
 });
 
-input.addEventListener('keypress',event=>{
-  if(event.key === 'Enter'){
-    onAdd();
-  }
-})
+function onAdd(){
+    const text = $input.value;
+    if (text === ''){
+        $input.focus();
+        return;
+    }
+    const item = make_row(text); 
+    $items.appendChild(item);
+    
+    item.scrollIntoView({block:'center'});//맨밑으로 스크롤
+    $input.value = '';
+    $input.focus();
+}
+function make_row(text){
+    const item_row = document.createElement('li'),
+            item_divider = document.createElement('div'),
+            item = document.createElement('div'),
+            item_name = document.createElement('span'),
+            item_delete = document.createElement('button');
+
+    item_row.setAttribute('class','item__row');
+    item_divider.setAttribute('class','item__divider');
+    item_row.appendChild(item);
+    item_row.appendChild(item_divider);
+
+    item.setAttribute('class','item');
+    item_name.setAttribute('class','item__name');
+    item_name.innerHTML = text;
+
+    item_delete.setAttribute('class','item__delete');
+    item_delete.innerHTML = `<i class="fas fa-regular fa-trash"></i>`;
+    item_delete.addEventListener('click',()=>{
+        $items.removeChild(item_row);
+    });
+    
+    item.appendChild(item_name);
+    item.appendChild(item_delete);
+    return item_row;
+}
